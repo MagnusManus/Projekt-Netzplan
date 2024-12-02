837ddf8b99637965fd3cf1d68e550da55723e8f5
Manu <pflaume1337@gmail.com>
2024-12-02 08:21:52 +0100

    Attribute erstellt, getter und setter geschrieben

---
+++ src/Arbeitspaket.java
@@ -0,0 +1,62 @@
+public class Arbeitspaket {
+    private String name = "";
+    private int fruehesterAnfangszeitpunkt;
+    private int fruehesterEndzeitpunkt;
+    private int spaetesterAnfangszeitpunkt;
+    private int spaetesterEndzeitpunkt;
+    private int gesamtpuffer;
+    private int freierPuffer;
+
+    public int getFruehesterAnfangszeitpunkt(){
+
+    }
+
+    public int getFruehesterEndzeitpunkt(){
+
+    }
+
+    public int getSpaetesterAnfangszeitpunkt(){
+
+    }
+
+    public int getSpaetesterEndzeitpunkt(){
+
+    }
+
+    public int getGesamtpuffer(){
+
+    }
+
+    public int getFreierPuffer(){
+
+    }
+
+    public String getName(){
+
+    }
+
+    public void setFruehesterAnfangszeitpunkt(){
+
+    }
+
+    public void setFruehesterEndzeitpunkt(){
+
+    }
+
+    public void setSpaetesterAnfangszeitpunkt(){
+
+    }
+
+    public void setSpaetesterEndzeitpunkt(){
+
+    }
+
+    public void setFreierPuffer(){
+
+    }
+
+    public void setGesamtpuffer(){
+
+    }
+
+}
