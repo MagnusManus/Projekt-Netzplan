6110eb5ac70b6e929ecae3a0c314c40932ef38f6
Manu <pflaume1337@gmail.com>
2024-12-02 10:17:17 +0100

    Attribute erstellt, getter und setter geschrieben

--- src/Arbeitspaket.java
+++ src/Arbeitspaket.java
@@ -1,101 +1,100 @@
-    public class Arbeitspaket {
-        private String name = "";
-        private int vorgang;
-        private int vorgaenger;
-        private int fruehesterAnfangszeitpunkt;
-        private int fruehesterEndzeitpunkt;
-        private int spaetesterAnfangszeitpunkt;
-        private int spaetesterEndzeitpunkt;
-        private int gesamtpuffer;
-        private int freierPuffer;
+public class Arbeitspaket {
+    private String name = "";
+    private int vorgang;
+    private int vorgaenger;
+    private int fruehesterAnfangszeitpunkt;
+    private int fruehesterEndzeitpunkt;
+    private int spaetesterAnfangszeitpunkt;
+    private int spaetesterEndzeitpunkt;
+    private int gesamtpuffer;
+    private int freierPuffer;
 
 
-        public Arbeitspaket(String name, int dauer) {
-//
-//    }
+    public Arbeitspaket(String name, int dauer) {
 
-   public Arbeitspaket(String name, int dauer, int vorgaenger) {
+    }
 
-            }
+    public Arbeitspaket(String name, int dauer, int vorgaenger) {
 
-   public Arbeitspaket(String name, int dauer, int vorgaenger, int vorgaenger2) {
+    }
 
-            }
+    public Arbeitspaket(String name, int dauer, int vorgaenger, int vorgaenger2) {
 
-////    public Arbeitspaket(String name, int dauer, int vorgaenger, int vorgaenger2, int vorgaenge3) {
-////
-////    }
-////
-////    public int getVorgaenger(){
-////
-////    }
+    }
+
+    public Arbeitspaket(String name, int dauer, int vorgaenger, int vorgaenger2, int vorgaenge3) {
+
+    }
+
+    public int getVorgaenger() {
+
+    }
 
 
+    public int getFruehesterAnfangszeitpunkt() {
 
-            public int getFruehesterAnfangszeitpunkt () {
+    }
 
-            }
+    public int getFruehesterEndzeitpunkt() {
 
-            public int getFruehesterEndzeitpunkt(){
+    }
 
-            }
+    public int getSpaetesterAnfangszeitpunkt() {
 
-            public int getSpaetesterAnfangszeitpunkt(){
+    }
 
-            }
+    public int getSpaetesterEndzeitpunkt() {
 
-            public int getSpaetesterEndzeitpunkt(){
+    }
 
-            }
+    public int getGesamtpuffer() {
 
-            public int getGesamtpuffer(){
+    }
 
-            }
+    public int getFreierPuffer() {
 
-            public int getFreierPuffer(){
+    }
 
-            }
+    public String getName() {
 
-            public String getName(){
+    }
 
-            }
+    public void setFruehesterAnfangszeitpunkt() {
 
-            public void setFruehesterAnfangszeitpunkt(){
+    }
 
-            }
+    public void setFruehesterEndzeitpunkt() {
 
-            public void setFruehesterEndzeitpunkt(){
+    }
 
-            }
+    public void setSpaetesterAnfangszeitpunkt() {
 
-            public void setSpaetesterAnfangszeitpunkt(){
+    }
 
-            }
+    public void setSpaetesterEndzeitpunkt() {
 
-            public void setSpaetesterEndzeitpunkt(){
+    }
 
-            }
+    public void setFreierPuffer() {
 
-            public void setFreierPuffer(){
+    }
 
-            }
+    public void setGesamtpuffer() {
 
-            public void setGesamtpuffer(){
+    }
 
-            }
+    //getter überladen für Berechnungen
+    public int getFruehesterAnfangszeitpunkt(int a, int b, int c) {
+        return Math.max(a, (b, c));
+    }
 
-            //getter überladen für Berechnungen
-            public int getFruehesterAnfangszeitpunkt(int a, int b, int c) {
-                return Math.max(a, (b, c));
-            }
+    public int getFruehesterAnfangszeitpunkt(int a, int b) {
 
-            public int getFruehesterAnfangszeitpunkt(int a, int b) {
+    }
 
-            }
+    public int getFruehesterAnfangszeitpunkt(int a) {
 
-            public int getFruehesterAnfangszeitpunkt(int a) {
+    }
 
-            }
-
-        }
 }
+
