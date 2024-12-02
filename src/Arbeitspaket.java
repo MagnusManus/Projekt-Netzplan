07f50cf6a58f72f099c344dc40d0e05ed969efff
Manu <pflaume1337@gmail.com>
2024-12-02 10:15:31 +0100

    Attribute erstellt, getter und setter geschrieben

---
+++ src/Arbeitspaket.java
@@ -0,0 +1,101 @@
+    public class Arbeitspaket {
+        private String name = "";
+        private int vorgang;
+        private int vorgaenger;
+        private int fruehesterAnfangszeitpunkt;
+        private int fruehesterEndzeitpunkt;
+        private int spaetesterAnfangszeitpunkt;
+        private int spaetesterEndzeitpunkt;
+        private int gesamtpuffer;
+        private int freierPuffer;
+
+
+        public Arbeitspaket(String name, int dauer) {
+//
+//    }
+
+   public Arbeitspaket(String name, int dauer, int vorgaenger) {
+
+            }
+
+   public Arbeitspaket(String name, int dauer, int vorgaenger, int vorgaenger2) {
+
+            }
+
+////    public Arbeitspaket(String name, int dauer, int vorgaenger, int vorgaenger2, int vorgaenge3) {
+////
+////    }
+////
+////    public int getVorgaenger(){
+////
+////    }
+
+
+
+            public int getFruehesterAnfangszeitpunkt () {
+
+            }
+
+            public int getFruehesterEndzeitpunkt(){
+
+            }
+
+            public int getSpaetesterAnfangszeitpunkt(){
+
+            }
+
+            public int getSpaetesterEndzeitpunkt(){
+
+            }
+
+            public int getGesamtpuffer(){
+
+            }
+
+            public int getFreierPuffer(){
+
+            }
+
+            public String getName(){
+
+            }
+
+            public void setFruehesterAnfangszeitpunkt(){
+
+            }
+
+            public void setFruehesterEndzeitpunkt(){
+
+            }
+
+            public void setSpaetesterAnfangszeitpunkt(){
+
+            }
+
+            public void setSpaetesterEndzeitpunkt(){
+
+            }
+
+            public void setFreierPuffer(){
+
+            }
+
+            public void setGesamtpuffer(){
+
+            }
+
+            //getter überladen für Berechnungen
+            public int getFruehesterAnfangszeitpunkt(int a, int b, int c) {
+                return Math.max(a, (b, c));
+            }
+
+            public int getFruehesterAnfangszeitpunkt(int a, int b) {
+
+            }
+
+            public int getFruehesterAnfangszeitpunkt(int a) {
+
+            }
+
+        }
+}
