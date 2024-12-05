2bc489fd2d15b55ce1cc00b963b84b6a770de67d
Manu <pflaume1337@gmail.com>
2024-12-05 19:34:06 +0100

    minor changes

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,9 @@ import java.util.logging.ErrorManager;
 public class Main {
     private Scanner sc;
     private String name;
+    private int dauer;
+    private ArrayList<Arbeitspaket> vorgaenger;
+    private int anzahlvorgaenger;
 
     public static void main(String[] args) {
         new Main();
@@ -17,11 +20,32 @@ public class Main {
 
 
     public Main() {
+        vorgaenger = new ArrayList<>();
         sc = new Scanner(System.in);
+        System.out.println("Bitte geben Sie den Namen des Arbeitspakets ein.");
         name = sc.nextLine();
 
         Arbeitspaket arbeitspaket = new Arbeitspaket();
         arbeitspaket.setName(name);
+
+        sc.next();
+
+        System.out.println("Bitte geben Sie die Dauer des Arbeitspakets ein.");
+        dauer = sc.nextInt();
+        arbeitspaket.setDauer(dauer);
+
+        sc.next();
+
+        System.out.println("Von wie vielen Arbeitspaketen (Vorgängern) hängt dieses Arbeitspaket ab?");
+        anzahlvorgaenger = sc.nextInt();
+
+        sc.next();
+
+        System.out.println("Nennen Sie den/ die Vorgänger: ");
+        for (int i = 0; i < anzahlvorgaenger; i++) {
+            System.out.printf("Noch %d Eingaben erwartet..." + "\n", anzahlvorgaenger - i);
+
+        }
 
 
 
